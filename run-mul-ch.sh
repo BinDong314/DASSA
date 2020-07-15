d7995e0a0bb686177bf1b0e6f934bdf5a2ed1bbc
dbin_sdm <dbin@lbl.gov>
2020-07-15 16:05:44 -0700

    add m-chan

--- run-mul-ch.sh
+++ run-mul-ch.sh
@@ -35,7 +35,6 @@ output_fn_tail=.h5
 MastChannelSet=(1 3 5 7)
 for MC in ${MastChannelSet[*]}
 do
-    change-ch.sh xcorrelation-test.config $MC ./westSac_180103000031_Xcorr_Master$MC.h5
     sed -i -e "s%\(master_index\).*%\1=$MC%"   $config_file
     sed -i -e "s%\(output_file\).*%\1=$output_fn_header$MC$output_fn_tail%"  $config_file
     
