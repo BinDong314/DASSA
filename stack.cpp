4e57f6ae05bb20ec3d72f7a500b3a37a8b61916a
dbin_sdm <dbin@lbl.gov>
2020-07-22 23:07:21 -0700

    add lts_per_file

--- stack.cpp
+++ stack.cpp
@@ -212,9 +212,19 @@ int main(int argc, char *argv[])
     semblance_denom_sum = new AU::Array<double>("EP_MEMORY", sc_size);
     coherency_sum = new AU::Array<std::complex<double>>("EP_MEMORY", sc_size);
     data_in_sum = new AU::Array<double>("EP_MEMORY", sc_size);
-    final_pwstack = new AU::Array<double>("EP_HDF5:" + stack_output_file_final_pwstack + ":" + stack_output_file_dataset_name, sc_size);
-    semblanceWeight = new AU::Array<double>("EP_HDF5:" + stack_output_file_semblanceWeight + ":" + stack_output_file_dataset_name, sc_size);
-    phaseWeight = new AU::Array<double>("EP_HDF5:" + stack_output_file_phaseWeight + ":" + stack_output_file_dataset_name, sc_size);
+
+    final_pwstack = new AU::Array<double>("EP_HDF5:" + stack_output_dir + "/" + stack_output_file_final_pwstack + ":" + stack_output_file_dataset_name, sc_size);
+    semblanceWeight = new AU::Array<double>("EP_HDF5:" + stack_output_dir + "/" + stack_output_file_semblanceWeight + ":" + stack_output_file_dataset_name, sc_size);
+    phaseWeight = new AU::Array<double>("EP_HDF5:" + stack_output_dir + "/" + stack_output_file_phaseWeight + ":" + stack_output_file_dataset_name, sc_size);
+
+    final_pwstack->EndpointControl(OP_DISABLE_COLLECTIVE_IO, "");
+    final_pwstack->EndpointControl(OP_DISABLE_MPI_IO, "");
+
+    semblanceWeight->EndpointControl(OP_DISABLE_COLLECTIVE_IO, "");
+    semblanceWeight->EndpointControl(OP_DISABLE_MPI_IO, "");
+
+    phaseWeight->EndpointControl(OP_DISABLE_COLLECTIVE_IO, "");
+    phaseWeight->EndpointControl(OP_DISABLE_MPI_IO, "");
 
     //semblanceWeight->Nonvolatile("EP_HDF5:./xcorr_examples_h5_stack_semblanceWeight.h5:/data");
     //phaseWeight->Nonvolatile("EP_HDF5:./xcorr_examples_h5_stack_phaseWeight.h5:/data");
@@ -276,7 +286,7 @@ int main(int argc, char *argv[])
 
         //PrintVector("semblance_denom_sum_v", semblance_denom_sum_v);
 
-        semblance_denom_sum->Nonvolatile("EP_HDF5:" + stack_output_file_semblance_denom_sum + ":" + stack_output_file_dataset_name);
+        semblance_denom_sum->Nonvolatile("EP_HDF5:" + stack_output_dir + "/" + stack_output_file_semblance_denom_sum + ":" + stack_output_file_dataset_name);
 
         for (int i = 0; i < chs_per_file * size_after_subset; i++)
         {
@@ -295,7 +305,7 @@ int main(int argc, char *argv[])
         }
         data_in_sum->WriteArray(H_start, H_end, data_in_sum_v);
 
-        data_in_sum->Nonvolatile("EP_HDF5:" + stack_output_file_data_in_sum_name + ":" + stack_output_file_dataset_name);
+        data_in_sum->Nonvolatile("EP_HDF5:" + stack_output_dir + "/" + stack_output_file_data_in_sum_name + ":" + stack_output_file_dataset_name);
 
         semblanceWeight->WriteArray(H_start, H_end, semblanceWeight_v);
         phaseWeight->WriteArray(H_start, H_end, phaseWeight_v);
