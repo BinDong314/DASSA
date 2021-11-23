9404ee98ff7ad6873f93efc7e763240ebe18aafc
Bin Dong <dbin@bins-mbp.attlocal.net>
2021-11-22 22:07:07 -0800

    Disable some output

--- xcorrelation.cpp
+++ xcorrelation.cpp
@@ -584,12 +584,15 @@ int main(int argc, char *argv[])
             file_range_index.push_back(i);
         }
         file_range_indexes_str = Vector2String(file_range_index);
-        if (!au_rank)
+        if (!ft_rank)
             std::cout << " file_range_indexes_str =" << file_range_indexes_str << "\n";
 
         std::vector<std::string> index_param;
         index_param.push_back(file_range_indexes_str);
         A->ControlEndpoint(DIR_FILE_SORT_INDEXES, index_param);
+
+        //index_param.clear();
+        //A->ControlEndpoint(DIR_SKIP_SIZE_CHECK, index_param);
     }
 
     if (!is_input_single_file)
@@ -619,7 +622,7 @@ int main(int argc, char *argv[])
         chunk_size[1] = array_size[1];
     }
 
-    if (!au_rank)
+    if (!ft_rank)
         PrintVector("chunk_size = ", chunk_size);
 
     //By default, the TDMS file is column major
