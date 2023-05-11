d1dba293b514d40bbd02f7631a3ab8ff3dad0426
dbinlbl <dbin@lbl.gov>
2023-05-11 15:36:18 -0700

    add CORR_DOT_PRODUCT_NO_DETREND

--- demo/plot.py
+++ demo/plot.py
@@ -4,8 +4,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #h5diff -d 0.000000000001 -v  
-file_org="/project/projectdirs/m1248/dassa-new/tmatch-verify-data/output/tmach-jun5-1740-2tpt.h5"
-file_reg="/project/projectdirs/m1248/dassa-new/tmatch-verify-data/output/tmach-reorg-jun5-1740-2tpt.h5"
+#file_org="/project/projectdirs/m1248/dassa-new/tmatch-verify-data/output/tmach-jun5-1740-2tpt.h5"
+#file_reg="/project/projectdirs/m1248/dassa-new/tmatch-verify-data/output/tmach-reorg-jun5-1740-2tpt.h5"
+
+file_org="/global/cfs/projectdirs/m1248/dassa-new/tmatch-verify-data/output/tmach-jun5-1740-2tpt.h5"
+file_reg="/global/cfs/projectdirs/m1248/dassa-new/tmatch-verify-data/output/tmach-reorg-jun5-1740-2tpt.h5"
+file_reg_smoothhw="/global/cfs/projectdirs/m1248/dassa-new/tmatch-verify-data/output/tmach-reorg-jun5-1740-2tpt-smoothhw.h5"
 
 f = h5py.File(file_reg, "r")
 data = f.get('/dat')[:]
@@ -17,6 +21,14 @@ plt.show()
 
 
 f = h5py.File(file_org, "r")
+data = f.get('/dat')[:]
+print("Values bigger than 0.5 =", data[data>0.5])
+print("Their indices are ", np.nonzero(data > 0.5))
+plt.plot(data)
+plt.show()
+
+
+f = h5py.File(file_reg_smoothhw, "r")
 data = f.get('/dat')[:]
 print("Values bigger than 0.5 =", data[data>0.5])
 print("Their indices are ", np.nonzero(data > 0.5))
